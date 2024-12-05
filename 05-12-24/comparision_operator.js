0435ad04501d3443f5f04a88054832c720ab701b
gourang001 <gourangchauhan001@gmail.com>
2024-12-05 14:53:41 +0530

    comparison and logical operator

---
+++ 05-12-24/comparision_operator.js
@@ -0,0 +1,28 @@
+// Comparison operators are used in logical statements to determine equality or difference between variables or values.
+let x=5;
+// equal to
+console.log(x==8); // false
+console.log(x==5); // true
+
+// equal value and equal type
+console.log(x==5); // true
+console.log(x==="5"); // false
+
+// not equal	
+console.log(x!=6); // true
+
+// not equal value or not equal type
+console.log(x!==8); // true
+console.log(x!=="5"); // true
+
+// greater than
+console.log(x>8); // false
+
+// less than
+console.log(x<8); // true
+
+// greater than or equal to
+console.log(x>=8); // false
+
+// less than or equal to
+console.log(x<=8); // true
